d7d95b3b310529da1c3574e5456091e4cb5921a6
supriya42 <ruchi1610shah@gmail.com>
2022-04-12 17:36:09 +0530

    commit 101

---
+++ com.crm.comcast.GitPractice/src/test/java/com/crm/comcast/GitPractice/ClassD.java
@@ -0,0 +1,13 @@
+package com.crm.comcast.GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassD 
+{
+	@Test
+	public void demoTest()
+	{
+		System.out.println("hello");
+		System.out.println("hi");
+	}
+}
