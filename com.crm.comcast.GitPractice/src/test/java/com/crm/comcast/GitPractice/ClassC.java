581f4757d32241d774160f345e2ebfef97dc8661
supriya42 <ruchi1610shah@gmail.com>
2022-04-12 13:18:48 +0530

    Fifth_Commit

--- com.crm.comcast.GitPractice/src/test/java/com/crm/comcast/GitPractice/ClassC.java
+++ com.crm.comcast.GitPractice/src/test/java/com/crm/comcast/GitPractice/ClassC.java
@@ -1,10 +1,14 @@
 package com.crm.comcast.GitPractice;
 
+import org.testng.annotations.Test;
+
 public class ClassC 
 {
+	@Test
 	public void newChange()
 	{
 		System.out.println("made new change and pushing to github");
 		System.out.println("check if you have made change or not");
+		System.out.println("hi sitesh");
 	}
 }
