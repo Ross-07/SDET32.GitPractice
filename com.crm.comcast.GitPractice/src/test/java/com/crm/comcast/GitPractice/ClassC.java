7effbd944783a67e50e95208850625e5a9730440
supriya42 <ruchi1610shah@gmail.com>
2022-04-12 12:47:09 +0530

    Third_commit

---
+++ com.crm.comcast.GitPractice/src/test/java/com/crm/comcast/GitPractice/ClassC.java
@@ -0,0 +1,10 @@
+package com.crm.comcast.GitPractice;
+
+public class ClassC 
+{
+	public void newChange()
+	{
+		System.out.println("made new change and pushing to github");
+		System.out.println("check if you have made change or not");
+	}
+}
