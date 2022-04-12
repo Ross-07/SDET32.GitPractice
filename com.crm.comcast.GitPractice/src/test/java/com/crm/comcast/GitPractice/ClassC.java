4f7db1bcc89b498c5da4238876d5acdd2a52dec5
supriya42 <ruchi1610shah@gmail.com>
2022-04-12 17:31:14 +0530

    commit msg

--- com.crm.comcast.GitPractice/src/test/java/com/crm/comcast/GitPractice/ClassC.java
+++ com.crm.comcast.GitPractice/src/test/java/com/crm/comcast/GitPractice/ClassC.java
@@ -2,16 +2,11 @@ package com.crm.comcast.GitPractice;
 
 import org.testng.annotations.Test;
 
-public class ClassC 
-{
+public class ClassC {
 	@Test
-	public void newChange()
+	public void demoTest2()
 	{
-		System.out.println("made new change and pushing to github");
-		System.out.println("check if you have made change or not");
-
-		System.out.println("hi sitesh how are you");
-		System.out.println("hi change accepted");
-		System.out.println("hello");
+		System.out.println("second Push");
+		System.out.println("third push");
 	}
 }
